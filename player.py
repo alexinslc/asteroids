8574dccf034586d377f59382a1ed5ef61619a625
alexinslc <alexinslc@gmail.com>
2025-04-29 16:37:08 -0600

    add rate limiting on shots

--- player.py
+++ player.py
@@ -8,18 +8,24 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def draw(self, screen):
         triangle = self.triangle()
         pygame.draw.polygon(screen, (255, 255, 255), triangle, 2)
 
     def shoot(self):
+      # Check if the weapon is on cooldown
+      if self.timer > 0:
+          # Cannot shoot yet
+          return None
+      # Weapon is ready to fire - Set the cooldown timer
+      self.timer = PLAYER_SHOOT_COOLDOWN
       forward = pygame.Vector2(0, 1).rotate(self.rotation)
       shot_position = self.position + forward * self.radius
       shot = Shot(shot_position.x, shot_position.y, SHOT_RADIUS)
       shot.velocity = forward * PLAYER_SHOOT_SPEED
       return shot
-
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -32,6 +38,12 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
+            # Update the shooting cooldown timer
+            if self.timer > 0:
+              self.timer -= dt
+            # Ensure timer doesn't go below 0
+            if self.timer < 0:
+              self.timer = 0
             shot = self.shoot()
         # Normalize rotation to be within 0-360 degrees
         self.rotation %= 360
